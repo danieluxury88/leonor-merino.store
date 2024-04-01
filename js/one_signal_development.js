1dd1303784e08c853d3d6ecfdc31e6d841358967
Daniel Proaño Chacón <danieluxury@gmail.com>
2024-04-01 03:20:04 -0500

    Update init configuration

--- js/one_signal_development.js
+++ js/one_signal_development.js
@@ -1,45 +1,40 @@
-function pushSubscriptionChangeListener(event) {
-  if (event.current.token) {
-    console.log(`The push subscription has received a token!`);
-    //this is a good place to call OneSignal.login and pass in your user ID
-  }
-}
-
-function slidedownEventListener(wasShown) {
-  console.log(`Slidedown shown event ${wasShown}`);
-}
-
-function permissionChangeListener(permission) {
-  if (permission) {
-    console.log(`permission accepted!`);
-  }
-}
-function notificationDismissedListener(event) {
-  console.log(`dismiss event: ${event}`);
-}
-function foregroundWillDisplayListener(event) {
-  console.log(`notification will display: ${notification}`);
-}
-function clickEventListener(event) {
-  console.log(`click event: ${event}`);
-}
-
-function eventListener(event) {
-  console.log(`${event}`);
-}
-
 console.log("OneSignal is loaded");
 window.OneSignalDeferred = window.OneSignalDeferred || [];
 
 OneSignalDeferred.push(function (OneSignal) {
   OneSignal.init({
     appId: "53a39ae0-5202-4e12-81ac-6dfb335a559f",
-    safari_web_id:
-      "web.onesignal.auto.175a5781-a0a7-4966-97e0-0bfa01fcdb1f",
+    safari_web_id: "web.onesignal.auto.175a5781-a0a7-4966-97e0-0bfa01fcdb1f",
+    persistNotification: false,
+    serviceWorkerParam: { scope: "./push/onesignal/" },
+    serviceWorkerPath: "./push/onesignal/OneSignalSDKWorker.js",
+
     notifyButton: {
-      enable: true,
+      enable: true, // Enable the Subscription Bell
+      size: "medium", // 'small', 'medium', or 'large'
+      position: "bottom-right", // 'bottom-left' or 'bottom-right'
+      prenotify: true, // Show an icon indicating unread messages before user subscribes
+      showCredit: false, // Hide the 'Powered by OneSignal' branding
+      text: {
+        "tip.state.unsubscribed": "Subscribe to notifications",
+        "tip.state.subscribed": "You're subscribed to notifications",
+        "tip.state.blocked": "You've blocked notifications",
+        "message.prenotify": "Click to subscribe to notifications",
+        "message.action.subscribed": "Thanks for subscribing!",
+        "message.action.resubscribed": "You're subscribed to notifications",
+        "message.action.unsubscribed": "You won't receive notifications again",
+        "dialog.main.title": "Manage Site Notifications",
+        "dialog.main.button.subscribe": "SUBSCRIBE",
+        "dialog.main.button.unsubscribe": "UNSUBSCRIBE",
+        "dialog.blocked.title": "Unblock Notifications",
+        "dialog.blocked.message":
+          "Follow these instructions to allow notifications:",
+      },
     },
     welcomeNotification: {
+      disable: false, // Set to true to disable the welcome notification
+      title: "Welcome!", // Custom title for welcome notification
+      message: "Thanks for subscribing to our notifications!", // Custom message for welcome notification
       delay: 10,
     },
     promptOptions: {
@@ -86,9 +81,6 @@ OneSignalDeferred.push(function (OneSignal) {
         ],
       },
     },
-    persistNotification: false,
-    serviceWorkerParam: { scope: "./push/onesignal/" },
-    serviceWorkerPath: "./push/onesignal/OneSignalSDKWorker.js",
   });
 });
 
@@ -180,3 +172,30 @@ OneSignalDeferred.push(function () {
 OneSignalDeferred.push(function () {
   OneSignal.Notifications.addEventListener("eventname", eventListener);
 });
+
+function pushSubscriptionChangeListener(event) {
+  if (event.current.token) {
+    console.log(`The push subscription has received a token!`);
+    //this is a good place to call OneSignal.login and pass in your user ID
+  }
+}
+function slidedownEventListener(wasShown) {
+  console.log(`Slidedown shown event ${wasShown}`);
+}
+function permissionChangeListener(permission) {
+  if (permission) {
+    console.log(`permission accepted!`);
+  }
+}
+function notificationDismissedListener(event) {
+  console.log(`dismiss event: ${event}`);
+}
+function foregroundWillDisplayListener(event) {
+  console.log(`notification will display: ${notification}`);
+}
+function clickEventListener(event) {
+  console.log(`click event: ${event}`);
+}
+function eventListener(event) {
+  console.log(`${event}`);
+}
